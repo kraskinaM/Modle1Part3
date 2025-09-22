83da02202892cc27a3701d05f10989f0e3799199
kraskinaM <kraskinamaria54@gmail.com>
2025-09-22 14:00:11 +0300

    Add files via upload

---
+++ Modui1Part3Task3.py
@@ -0,0 +1,16 @@
+meters = float(input("10"))
+
+centimeters = meters * 100
+decimeters = meters * 10
+milimeters = meters * 1000
+miles = meters * 0.000621371
+
+print( f"{meters} = {centimeters} ")
+print( f"{meters}  = {decimeters} ")
+print( f"{meters} = {milimeters} ")
+print( f"{meters} = {miles} ")
+print( f"{meters} = {miles} ")
+
+
+
+
