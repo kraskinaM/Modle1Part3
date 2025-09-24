b2876151e487db28091d0cce5f64cd0408f56f4a
kraskinaM <kraskinamaria54@gmail.com>
2025-09-24 13:56:30 +0300

    Update Modui1Part3Task3.py

--- Modui1Part3Task3.py
+++ Modui1Part3Task3.py
@@ -1,16 +1,15 @@
-meters = float(input("10"))
+meters = float(input())
 
 centimeters = meters * 100
 decimeters = meters * 10
 milimeters = meters * 1000
 miles = meters * 0.000621371
 
-print( f"{meters} = {centimeters} ")
-print( f"{meters}  = {decimeters} ")
+print( f"В {meters} метрах = {centimeters} сантиметров")
+print( f"{meters} = {decimeters} ")
 print( f"{meters} = {milimeters} ")
 print( f"{meters} = {miles} ")
 print( f"{meters} = {miles} ")
-
 
 
 
